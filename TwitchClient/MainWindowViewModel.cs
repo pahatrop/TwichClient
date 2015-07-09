ab29e2e1399d25e23ca70eae2c2ad07c00befbf9
pahatrop <pahatrop@gmail.com>
2015-07-09 22:07:15 +0400

    Create project

---
+++ TwitchClient/MainWindowViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Net;
+using System;
+using System.Windows;
+using System.ComponentModel;
+
+namespace TwitchClient
+{
+    public class Game
+    {
+        public string Channels { get; set; }
+        public string Viewers { get; set; }
+        public string Name { get; set; }
+        public Game(string channels, string viewers, string name)
+        {
+            Channels = channels;
+            Viewers = viewers;
+            Name = name;
+        }
+    }
+
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private List<Game> allGames;// = new List<Game>();
+        private List<Game> cg = new List<Game>();
+        public List<Game> AllGames { get { return allGames; } }
+        private int selectedindex = 0;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public int SelectedIndex
+        {
+            get { return selectedindex; }
+            set
+            {
+                if (value != -1)
+                {
+                    selectedindex = value;
+                }
+            }
+        }
+        public MainWindowViewModel()
+        {
+            WebRequest request = WebRequest.Create("https://api.twitch.tv/kraken/games/top");
+            request.BeginGetResponse(new AsyncCallback(OnResponse), request);
+            RaisePropertyChanged("AllGames");
+            RaisePropertyChanged("SelectedIndex");
+        }
+        private void UpdateList(MainModel mm)
+        {
+            foreach (TopModel currenGame in mm.top)
+            {
+                cg.Add(new Game("Channels: " + currenGame.channels.ToString(), "Viewers: " + currenGame.viewers.ToString(), currenGame.game.name ));
+            }
+            allGames = cg;
+            RaisePropertyChanged("AllGames");
+        }
+        public void OnResponse(IAsyncResult ar)
+        {
+            string text = "";
+            WebRequest request = (WebRequest)ar.AsyncState;
+            WebResponse response = request.EndGetResponse(ar);
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                    text += line + "\n";
+            }
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(MainModel));
+            MemoryStream ms = new MemoryStream(System.Text.ASCIIEncoding.ASCII.GetBytes(text));
+            MainModel mm = (MainModel)js.ReadObject(ms);
+            UpdateList(mm);
+        }
+    }
+}
---
+++ TwitchClient/Model.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace TwitchClient
+{
+    [DataContract]
+    public class MainModel
+    {
+        [DataMember]
+        public int _total;
+        [DataMember]
+        public LinkModel _links;
+        [DataMember]
+        public List<TopModel> top;
+    }
+     [DataContract]
+    public class LinkModel
+    {
+         [DataMember]
+        public string self;
+         [DataMember]
+        public string next;
+    }
+     [DataContract]
+    public class TopModel
+    {
+         [DataMember]
+        public int viewers;
+         [DataMember]
+        public int channels;
+         [DataMember]
+        public GameModel game;
+    }
+     [DataContract]
+    public class GameModel
+    {
+         [DataMember]
+        public string name;
+         /*[DataMember]
+        public int _id;
+         [DataMember]
+        public int giantbomb_id;
+         [DataMember]
+        public BoxModel box;
+         [DataMember]
+        public LogoModel logo;*/
+    }
+    public class ImageModel
+    {
+        [DataMember]
+        public string large;
+        [DataMember]
+        public string medium;
+        [DataMember]
+        public string small;
+        [DataMember]
+        public string template;
+    }
+     [DataContract]
+    public class BoxModel : ImageModel
+    {
+    }
+     [DataContract]
+    public class LogoModel : ImageModel
+    {
+    }
+}
